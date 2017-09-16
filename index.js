ddddfa302dd5cc7e6ff4786f86da6d120f36a01f
Sha Alibhai <shalotelli@gmail.com>
2017-09-16 10:57:05 -0400

    move config to .env file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const Config = require('./lib/config');
+
 const Server = require('./lib/server');
 const Environment = require('./lib/environment');
 const Token = require('./lib/token');
+
+module.exports.Config = Config;
 
 module.exports.Server = new Server;
 module.exports.Environment = new Environment;
