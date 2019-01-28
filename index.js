39e32c6a85ff719980648e2206d54837a739c036
Dhirendra <xyz.dhirendra24@gmail.com>
2019-01-28 21:05:48 +0530

    IN-38: Add support for logging and sentry.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Config = require('./lib/config');
-
+const {logger} = require('./lib/logging');
 const Server = require('./lib/server');
 const Environment = require('./lib/environment');
 const Token = require('./lib/token');
 
 module.exports.Config = Config;
-
+module.exports.logger = logger;
 module.exports.Server = new Server;
 module.exports.Environment = new Environment;
 module.exports.Token = new Token;
