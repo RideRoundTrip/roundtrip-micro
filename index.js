efee49d8ea8bac20e4370e4b25a2276be92ba697
Dhirendra <xyz.dhirendra24@gmail.com>
2019-05-10 15:31:52 +0530

    IN-231: Add authentication middleware.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const {logger} = require('./lib/logging');
 const Server = require('./lib/server');
 const Environment = require('./lib/environment');
 const Token = require('./lib/token');
+const exceptions = require('./lib/exceptions');
+const middlewares = require('./lib/middlewares');
 
 module.exports.Config = Config;
 module.exports.logger = logger;
 module.exports.Server = new Server;
 module.exports.Environment = new Environment;
 module.exports.Token = new Token;
+module.exports.exceptions = exceptions;
+module.exports.middlewares = middlewares;
